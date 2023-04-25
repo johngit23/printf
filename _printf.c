6d54f66b83577f18730ac83d0875a2c0cf9fbfbc
Yohannes <jhon.klaus2008@gmail.com>
2023-04-25 12:36:28 -0700

    updating files

--- _printf.c
+++ _printf.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <stdarg.h>
 
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
+ *
  * @format: format.
+ *
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
