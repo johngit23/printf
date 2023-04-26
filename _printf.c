8e06153d2fdc118b3d64bac64b4f350f159373cf
Yohannes <jhon.klaus2008@gmail.com>
2023-04-26 07:38:27 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,50 +1,71 @@
 #include "main.h"
-#include <limits.h>
-#include <stdio.h>
+
+void print_buffr(char buffr[], int *buffr_indx);
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - Custom Printf function
+ * by Jay B and Derrick Crowis
+ * @format: Value of format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buffr_indx = 0;
+	va_list list;
+	char buffr[BFR_SYZ];
 
-	register int count = 0;
+	return (format == NULL ? -1 : 0);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (*p == '%')
+		if (format[i] != '%')
 		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			buffr[buffr_indx++] = format[i];
+			if (buffr_indx == BFR_SYZ)
+				print_buffr(buffr, &buffr_indx);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffr(buffr, &buffr_indx);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = print_handler(format, &i, list, buffr,
+				flags, width, precision, size);
+			printed = (printed == -1) ? -1 : 0;
+			printed_chars += printed;
+		}
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+
+	print_buffr(buffr, &buffr_indx);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+
+/**
+ * print_buffr - Prints the contents of the buffer if it exists
+ * @buffr: Array of chars
+ * @buffr_indx: Index of characters and array size.
+ */
+void print_buffr(char buffr[], int *buffr_indx)
+{
+	if (*buffr_indx == BFR_SYZ)
+	{
+		print_buffr(buffr, buffr_indx);
+	}
+	else
+	{
+		*buffr_indx = 0;
+	}
 
 }
