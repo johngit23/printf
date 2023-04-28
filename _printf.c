434cf149109ecb92886a073b82d586d357fba862
Yohannes <jhon.klaus2008@gmail.com>
2023-04-28 01:19:15 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,51 +1,79 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ * Return: number of digits printed
+ */
+int print_number(int n)
+{
+    int digits = 0;
+
+    if (n == 0)
+    {
+        _putchar('0');
+        return (1);
+    }
+    else if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+        digits++;
+    }
+
+    while (n != 0)
+    {
+        int digit = n % 10;
+        _putchar(digit + '0');
+        n /= 10;
+        digits++;
+    }
+
+    return (digits);
+}
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
+ * _printf - prints a formatted string to the standard output
+ * @format: string containing format specifiers
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+    va_list args;
+    int i, count = 0;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++; /* skip the % character */
+
+            /* handle format specifier */
+            switch (format[i])
+            {
+                case 'd':
+                case 'i':
+                    count += print_number(va_arg(args, int));
+                    break;
+                default:
+                    _putchar('%');
+                    _putchar(format[i]);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            _putchar(format[i]);
+            count++;
+        }
+    }
+
+    va_end(args);
+
+    return (count);
 }
